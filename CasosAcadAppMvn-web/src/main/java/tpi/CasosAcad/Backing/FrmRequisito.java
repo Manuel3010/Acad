2cc059cb687fbcda418051fea743a9581eadaca3
manuel <manuel@Manuel>
2017-04-05 23:05:07 -0600

    5-abril-23:04 -- CRUD Requisito. No modifica el tipo requisito

--- CasosAcadAppMvn-web/src/main/java/tpi/CasosAcad/Backing/FrmRequisito.java
+++ CasosAcadAppMvn-web/src/main/java/tpi/CasosAcad/Backing/FrmRequisito.java
@@ -10,8 +10,12 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import org.primefaces.model.LazyDataModel;
@@ -31,18 +35,22 @@ public class FrmRequisito implements Serializable {
 
     private LazyDataModel<Requisito> modeloRequisito;
     private LazyDataModel<TipoRequisito> modeloTipo;
-    private Requisito registro;
+    private Requisito registro = new Requisito();
     private TipoRequisito tipo;
+    private List<TipoRequisito> tipos;
     
     
     @EJB
     private RequisitoFacadeLocal rfl;
-   /* @EJB
+    @EJB
     private TipoRequisitoFacadeLocal trfl;
-    */
+    
     
     @PostConstruct
     public void init(){
+        
+             this.tipos= trfl.findAll();
+        
              setModeloRequisito(new LazyDataModel<Requisito>(){
 
             @Override
@@ -78,9 +86,112 @@ public class FrmRequisito implements Serializable {
                 return null;
             }       
         });
+
+             
+             setModeloTipo(new LazyDataModel<TipoRequisito>(){
+
+            @Override
+            public List<TipoRequisito> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+                List salida = new ArrayList();
+                if(trfl != null){
+                    this.setRowCount(trfl.count());
+                    int[] rango = new int[2];
+                    rango[0] = first;
+                    rango[1] = pageSize;
+                    salida = trfl.findRange(rango);
+                }
+                return salida;
+            }
+
+            @Override
+            public Object getRowKey(TipoRequisito object) {
+                return object.getIdTipoRequisito();
+            }
+
+            @Override
+            public TipoRequisito getRowData(String rowKey) {
+                if(this.getWrappedData()!=null){
+                    List<TipoRequisito> lista = (List<TipoRequisito>) this.getWrappedData();
+                    if(!lista.isEmpty()) {
+                        for(TipoRequisito get : lista) {
+                            if(get.getIdTipoRequisito().compareTo(Integer.parseInt(rowKey))==0) {
+                                return get;
+                            }
+                        }
+                    }
+                }
+                return null;
+            }       
+        });
+             
+             
+    }
+    
+     public Integer getTipoSeleccionado(){
+     if(registro!= null){
+            if(registro.getIdTipoRequisito()!= null){
+                return this.registro.getIdTipoRequisito().getIdTipoRequisito();
+            } else {
+                return null;
+            }         
+        } else {
+            return null;
+        }
+    }
+    
+    public void setTipoSeleccionado(Integer idTipo){
+        if(idTipo >= 0 && !this.tipos.isEmpty()){
+            for(TipoRequisito tre : this.getTipos()) {
+                if(Objects.equals(tre.getIdTipoRequisito(), idTipo)) {
+                    if(this.registro.getIdTipoRequisito() != null) {
+                        this.registro.getIdTipoRequisito().setIdTipoRequisito(idTipo);
+                    } else {
+                        this.registro.setIdTipoRequisito(tre);
+                    }
+                }
+            }
+        }
     
     }
     
+    
+          public void btnGuardarAction(ActionEvent ae){
+        try {    
+            if(this.registro != null && this.rfl != null){
+                boolean resultado = this.rfl.create(registro);
+                FacesMessage msj = new FacesMessage(FacesMessage.SEVERITY_INFO, resultado?"Creado con exito":"Error", null);
+                //this.agregar = !resultado;
+                FacesContext.getCurrentInstance().addMessage(null, msj);
+            }
+        } catch (Exception e) {
+           
+        }
+     
+      }
+     
+     
+      public void btnModificarAction(ActionEvent ae){
+        try{
+            boolean resultado = this.rfl.editar(registro); 
+            FacesMessage msj = new FacesMessage(FacesMessage.SEVERITY_INFO, resultado?"Modificado con exito":"Error", null);
+            //this.editar = resultado;
+            FacesContext.getCurrentInstance().addMessage(null, msj);
+        }catch(Exception e){
+            System.err.println(""+e);
+        }
+    }
+
+            public void btnEliminarAction(ActionEvent ae) {
+        try {
+            if(this.registro != null && this.rfl != null){
+                boolean resultado = this.rfl.remove(registro);
+                FacesMessage msj = new FacesMessage(FacesMessage.SEVERITY_INFO, resultado?"Eliminado con exito":"Error", null);
+                FacesContext.getCurrentInstance().addMessage(null, msj);
+            }
+        } catch (Exception e) {
+        }
+    }
+
     
     
     /**
@@ -119,6 +230,14 @@ public class FrmRequisito implements Serializable {
 
     public void setTipo(TipoRequisito tipo) {
         this.tipo = tipo;
+    }
+
+    public List<TipoRequisito> getTipos() {
+        return tipos;
+    }
+
+    public void setTipos(List<TipoRequisito> tipos) {
+        this.tipos = tipos;
     }
     
 }
