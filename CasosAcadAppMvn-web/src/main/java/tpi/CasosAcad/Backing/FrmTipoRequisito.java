5e69c7f7435c6f426e7d629adf5fed29a2710602
manuel <manuel@Manuel>
2017-04-19 12:18:56 -0600

    19-04-12:08pm -- Limpia formularios de Tipo Requisito despues de agregar eliminar y nuevo.

--- CasosAcadAppMvn-web/src/main/java/tpi/CasosAcad/Backing/FrmTipoRequisito.java
+++ CasosAcadAppMvn-web/src/main/java/tpi/CasosAcad/Backing/FrmTipoRequisito.java
@@ -16,6 +16,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
+import org.primefaces.context.RequestContext;
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortOrder;
 import tpi.CasosAcad.Entidades.TipoRequisito;
@@ -32,7 +33,7 @@ public class FrmTipoRequisito implements Serializable {
     private LazyDataModel<TipoRequisito> modelo;
     @EJB
     private TipoRequisitoFacadeLocal trfl;
-    private TipoRequisito tipo= new TipoRequisito();
+    private TipoRequisito tipo;//= new TipoRequisito();
     private boolean editar;
     private boolean agregar;
     public TipoRequisito getTipo() {
@@ -46,7 +47,6 @@ public class FrmTipoRequisito implements Serializable {
     
     @PostConstruct
     public void init(){
-        //this.tipo=new TipoRequisito();
         
          setModelo(new LazyDataModel<TipoRequisito>(){
 
@@ -89,29 +89,34 @@ public class FrmTipoRequisito implements Serializable {
     }
     
     
-     public void btnNuevoAction(ActionEvent ae) {
-       //  this.agregar= true;
-         //this.editar = false;
-        // this.tipo= new TipoRequisito();
-        //this.agregar= true;
-        try{
+    public void limpiar(){
+          RequestContext.getCurrentInstance().reset("vistaEditar");
             this.tipo = new TipoRequisito();
+    }
+    
+     public void btnNuevoAction(ActionEvent ae) {
+        editar=false;
+         try{
+            limpiar();
+         
         }catch(Exception e){
             
         }
     }
      
+   
      
       public void btnGuardarAction(ActionEvent ae){
         try {
-        //    
+            
             if(this.tipo != null && this.trfl != null){
-                //this.tipo=new TipoRequisito();
+                
                 boolean resultado = this.trfl.create(tipo);
-                //this.tipo=new TipoRequisito();
+        
                 FacesMessage msj = new FacesMessage(FacesMessage.SEVERITY_INFO, resultado?"Creado con exito":"Error", null);
                 this.agregar = !resultado;
                 FacesContext.getCurrentInstance().addMessage(null, msj);
+                limpiar();
             }
         } catch (Exception e) {
            
@@ -126,36 +131,21 @@ public class FrmTipoRequisito implements Serializable {
             FacesMessage msj = new FacesMessage(FacesMessage.SEVERITY_INFO, resultado?"Modificado con exito":"Error", null);
             this.editar = resultado;
             FacesContext.getCurrentInstance().addMessage(null, msj);
+            limpiar();
         }catch(Exception e){
             System.err.println(""+e);
         }
     }
-      
-      public void guardar(){
-      
-      try {
-        //    this.tipo=new TipoRequisito();
-            if(this.tipo != null && this.trfl != null){
-                boolean resultado = this.trfl.create(tipo);
-                this.tipo=new TipoRequisito();
-                FacesMessage msj = new FacesMessage(FacesMessage.SEVERITY_INFO, resultado?"Creado con exito":"Error", null);
-                this.agregar = !resultado;
-                FacesContext.getCurrentInstance().addMessage(null, msj);
-            }
-        } catch (Exception e) {
-            FacesMessage msj = new FacesMessage(FacesMessage.SEVERITY_FATAL, e.getMessage(), null);
-            FacesContext.getCurrentInstance().addMessage(null, msj);
-        }
-      }
-       
-    
-    
+                   
         public void btnEliminarAction(ActionEvent ae) {
         try {
             if(this.tipo != null && this.trfl != null){
                 boolean resultado = this.trfl.remove(tipo);
+                
                 FacesMessage msj = new FacesMessage(FacesMessage.SEVERITY_INFO, resultado?"Eliminado con exito":"Error", null);
                 FacesContext.getCurrentInstance().addMessage(null, msj);
+                limpiar();
+                
             }
         } catch (Exception e) {
         }
@@ -165,17 +155,7 @@ public class FrmTipoRequisito implements Serializable {
      public void cambioTabla(){
         this.editar = true;
     }
-     
-     public void agregarNuevo(){
-     //this.agregar= true;
-     //this.editar= false;
-      this.tipo= new TipoRequisito();
-     
-     }
-    
-    
-    
-
+         
     public LazyDataModel<TipoRequisito> getModelo() {
         return modelo;
     }
@@ -203,6 +183,8 @@ public boolean isEditar() {
      * Creates a new instance of FrmTipoRequisito
      */
     public FrmTipoRequisito() {
+        this.tipo= new TipoRequisito();
+        
     }
     
 }
