362299753ae7e782858a332eec5cfce6008d2a62
sigfridGom <sigfridgom@gmail.com>
2017-04-26 11:22:41 -0600

    SE AGREGO ESQUELETO COMPLETO PROCESO YA ES FUNCIONAL DETALLES DE VALIDACION CON AJAX Y HIERBAS COMMIT DE PRUEBA PARA MANUEL

---
+++ CasosAcadAppMvn-web/src/main/java/tpi/CasosAcad/Recursos/procesoRecurso.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpi.CasosAcad.Recursos;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import tpi.CasosAcad.Entidades.Proceso;
+import tpi.CasosAcad.Sessions.ProcesoFacadeLocal;
+
+/**
+ *
+ * @author manuel
+ */
+
+@RequestScoped
+@Path("Proceso")
+
+public class procesoRecurso {
+    
+    
+    @Inject
+    private ProcesoFacadeLocal ProcesoFacade;
+    
+    
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Proceso> findAll(){
+    
+    List salida= new ArrayList();
+    try{
+       if(ProcesoFacade!=null){
+       
+       salida=ProcesoFacade.findAll();
+       
+       }
+                    
+    }catch(Exception e){
+    
+    
+    }
+    return salida;
+    
+    }    
+    
+    
+    
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Proceso findById(@PathParam("id")final Integer id){
+    
+        Proceso salida= new Proceso();
+        try{
+        if(ProcesoFacade!=null){
+        salida= ProcesoFacade.find(id);
+        
+        }
+        
+        }catch(Exception e){
+        
+        }
+    
+    return salida;    
+    }    
+    
+    
+    
+    
+    
+}
