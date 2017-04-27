86b5e30de82e6dec119906361394caf675b8e5a6
sigfridGom <sigfridgom@gmail.com>
2017-04-27 16:12:49 -0600

    SE AGREGO ESQUELETO COMPLETO PROCESO, SOLICITUD, CASO YA ES FUNCIONAL DETALLES DE VALIDACION CON AJAX Y OTRAS HIERBAS

--- CasosAcadAppMvn-web/src/main/java/tpi/CasosAcad/Backing/FrmRequisito.java
+++ CasosAcadAppMvn-web/src/main/java/tpi/CasosAcad/Backing/FrmRequisito.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -254,6 +254,8 @@ public class FrmRequisito implements Serializable {
         this.registro = registro;
     }
 
+    
+    
     public TipoRequisito getTipo() {
         return tipo;
     }
--- CasosAcadAppMvn-web/src/main/java/tpi/CasosAcad/Recursos/casoRecurso.java
+++ CasosAcadAppMvn-web/src/main/java/tpi/CasosAcad/Recursos/casoRecurso.java
@@ -5,10 +5,79 @@
  */
 package tpi.CasosAcad.Recursos;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import tpi.CasosAcad.Entidades.Caso;
+import tpi.CasosAcad.Sessions.CasoFacadeLocal;
+
 /**
  *
- * @author sigfrid
+ * @author manuel
  */
-public class casoRecurso {
+@RequestScoped
+@Path("caso")
+public class casoRecurso  {
+    
+    
+    @Inject
+    private CasoFacadeLocal CasoFacade;
+    
+    
+     @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Caso> findAll(){
+    
+    List salida= new ArrayList();
+    try{
+       if(CasoFacade!=null){
+       
+       salida=CasoFacade.findAll();
+       
+       }
+                    
+    }catch(Exception e){
+    Logger.getLogger(getClass().getName()).log(Level.SEVERE,e.getMessage(),e);
+    
+    }
+    return salida;
+    
+    }    
+    
+    
+    
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Caso findById(@PathParam("id")final Integer id){
+    
+        Caso salida= new Caso();
+        try{
+        if(CasoFacade!=null){
+        salida= CasoFacade.find(id);
+        
+        }
+        
+        }catch(Exception e){
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE,e.getMessage(),e);
+        }
+    
+    return salida;    
+    }    
+    
+    
+    
+    
+    
+    
     
 }
+
